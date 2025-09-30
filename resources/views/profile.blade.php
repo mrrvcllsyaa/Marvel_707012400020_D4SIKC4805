9bb4531a1accc73c6f5fd6f458df004868843d73
Marvel Callysa Rorong <marvelcallysa@gmail.com>
2025-09-30 13:14:33 +0700

    Modul 2

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h1>Profile Page</h1>
+<p>Nama    : Marvel Callysa </p>
+<p>Jurusan : D4 Sistem Informasi Kota Cerdas </p>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-//use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +13,30 @@
 |
 */
 
-Route::get('/fakultas', function () {
-    $fakultas = [
-        ['kode' => 'FRI', 'nama_fakultas' => 'Fakultas Rekayasa Industri'],
-        ['kode' => 'FTE', 'nama_fakultas' => 'Fakultas Teknik Elektro'],
-        ['kode' => 'FIF', 'nama_fakultas' => 'Fakultas Informatika'],
-        ['kode' => 'FEB', 'nama_fakultas' => 'Fakultas Ekonomi dan Bisnis'],
-        ['kode' => 'FIK', 'nama_fakultas' => 'Fakultas Industri Kreatif'],
-        ['kode' => 'FIT', 'nama_fakultas' => 'Fakultas Ilmu Terapan'],
-    ];
-        return view('fakultas', ['fakultas' => $fakultas]);
+Route::get('/', function () {
+        return view('welcome');
 });
+
+Route::get('/hello', function () {
+        return'Hello, Marvel';
+});
+
+Route::get('/user/{name}', function ($name) {
+        return "Nama Saya $name";
+});
+
+Route::get('/greet/{name?}', function ($name = 'Guest') {
+        return "Halo, $name";
+});
+
+Route::get('/profile', function () {
+        return view ('profile');
+});
+
+Route::get('/about', function () {
+        return view ('about', ['name' => 'Marvel']);
+});
+
+Route::get('/home', function () {
+        return 'Halo, Ini adalah halaman home';
+})->name('home.page');
